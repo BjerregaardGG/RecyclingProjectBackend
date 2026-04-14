0992ebd2cd242249ffb3d0a7bede8c8456af4434
Oliver Bjerregaard <oliverbjerregaard1@gmail.com>
2026-04-14 12:51:55 +0200

    Initial commit

---
+++ src/main/java/com/recyclingprojectbackend/RecyclingProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.recyclingprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecyclingProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecyclingProjectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recyclingprojectbackend/RecyclingProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recyclingprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecyclingProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
